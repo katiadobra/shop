05f545834ff8b752022c5a83d75fca2b9c0892b8
katiadobra <broun.kat@gmail.com>
2020-10-06 23:33:38 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to the Shop</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
