32450ff728ae49516f793ed42a0559553285349e
katiadobra <broun.kat@gmail.com>
2020-10-07 21:33:27 +0200

    adding bootstrap, Header, Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <main>
-        <h1>Welcome to the Shop</h1>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to the Shop</h1>
+        </Container>
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-  return <Footer>Footer</Footer>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; Minty Shop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
