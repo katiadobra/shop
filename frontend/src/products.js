3ce06755cad3da38912ffe1370e9a70a5834fecf
katiadobra <broun.kat@gmail.com>
2020-10-07 23:31:17 +0200

    adding Product screen

---
+++ frontend/src/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Macarons Dunkle Schokolade',
+    image: '/images/default.jpeg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Macarons Pistazie',
+    image: '/images/default.jpeg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 2.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Macarons Caramel au beurre salé',
+    image: '/images/default.jpeg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 1.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Macarons Zitrone',
+    image: '/images/default.jpeg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 1.5,
+    countInStock: 1,
+    rating: 5,
+    numReviews: 12,
+  },
+];
+
+export default products;
