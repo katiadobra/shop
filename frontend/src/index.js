3eefeee9da996cbf92858748c74614815814d9b0
katiadobra <broun.kat@gmail.com>
2020-10-11 22:11:41 +0200

    Create a Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './styles/bootstrap.min.css';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
