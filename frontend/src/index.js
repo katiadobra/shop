7062018c04a4fac2d653f18f7d75fca25762a3ae
katiadobra <broun.kat@gmail.com>
2020-10-12 01:15:29 +0200

    Product List Reducer & Action

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/store';
 import './styles/bootstrap.min.css';
 import './styles/index.css';
 import App from './App';
