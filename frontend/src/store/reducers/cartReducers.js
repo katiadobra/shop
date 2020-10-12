4597e7a231ad6128eb8a5524f13667d11fda0360
katiadobra <broun.kat@gmail.com>
2020-10-12 14:00:33 +0200

    Qty Select & Add To Cart Button

---
+++ frontend/src/store/reducers/cartReducers.js
@@ -0,0 +1,8 @@
+import { CART_ADD_ITEM } from '../constants';
+
+export const cartReducer = (state = { cartItem: [] }, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
