1ccfe4a0b4c3e4e7d1bd356e16827b96bbb23d29
katiadobra <broun.kat@gmail.com>
2020-10-20 23:11:58 +0200

    User Registration & Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// the plaintext password will be encrypted through
+// this piece of middleware
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
