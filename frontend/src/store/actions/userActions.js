27ab55f817195b1aa10cabf5d243f86fdd67c840
katiadobra <broun.kat@gmail.com>
2020-10-21 22:09:05 +0200

    User Login Screen & Functionality

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -21,7 +21,7 @@ export const login = (email, password) => async (dispatch) => {
 
     const { data } = await axios.post(
       '/api/users/login',
-      { email, pssword },
+      { email, password },
       config
     );
 
