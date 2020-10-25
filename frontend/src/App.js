de8936a8ecb9365a10bd9a039545980f8e5748d9
katiadobra <broun.kat@gmail.com>
2020-10-25 23:45:29 +0100

    Payment Screen & Save Payment Method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Container>
           <Route path="/register" component={RegisterScreen} exact />
           <Route path="/shipping" component={ShippingScreen} exact />
+          <Route path="/payment" component={PaymentScreen} exact />
           <Route path="/login" component={LoginScreen} exact />
           <Route path="/profile" component={ProfileScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
